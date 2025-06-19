e5f39afb4445b5ee680ace6cdbf894b1c22c9c0c
DIOURI Yassine <yassinediouri92@gmail.com>
2025-06-19 03:04:48 +0200

    FEAT[US-1] Expose deposit operation via AccountController + Add exception handler for IllegalArgument

--- bff/src/main/java/yas/dio/katas/bankaccount/account/AccountController.java
+++ bff/src/main/java/yas/dio/katas/bankaccount/account/AccountController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class AccountController {
     @GetMapping("/{id}")
     public double getBalance(@PathVariable final Long id) {
         return accountService.getBalance(id);
+    }
+
+    @PutMapping("/{id}")
+    public void deposit(@PathVariable final Long id, @RequestBody final double amount) {
+        accountService.deposit(id, amount);
     }
 }
