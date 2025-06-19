f5dc7014462a6517dfade1e3fb4ad3fae79c8655
DIOURI Yassine <yassinediouri92@gmail.com>
2025-06-19 10:27:41 +0200

    FEAT[US-3] Implement TransactionServiceImpl save method to could save transaction to db using transactionRepository

--- bff/src/main/java/yas/dio/katas/bankaccount/transaction/impl/TransactionServiceImpl.java
+++ bff/src/main/java/yas/dio/katas/bankaccount/transaction/impl/TransactionServiceImpl.java
@@ -1,14 +1,29 @@
 package yas.dio.katas.bankaccount.transaction.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import yas.dio.katas.bankaccount.account.Account;
+import yas.dio.katas.bankaccount.transaction.Transaction;
+import yas.dio.katas.bankaccount.transaction.TransactionRepository;
 import yas.dio.katas.bankaccount.transaction.TransactionService;
 
+import java.time.LocalDateTime;
+
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
 
+    private final TransactionRepository transactionRepository;
+
     @Override
-    public void save(Account account, double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void save(final Account account, final double amount) {
+        transactionRepository.save(
+                Transaction.builder()
+                        .date(LocalDateTime.now())
+                        .amount(amount)
+                        .balance(account.getBalance())
+                        .account(account)
+                        .build()
+        );
     }
 }
